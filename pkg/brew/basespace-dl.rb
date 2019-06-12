230a5b8a5326d73a75d7860017b88f43f1e6f412
daniel <daniel@daniels-MacBook-Pro.local>
2019-06-11 23:15:50 -0500

    typo in brew

--- pkg/brew/basespace-dl.rb
+++ pkg/brew/basespace-dl.rb
@@ -3,10 +3,10 @@ class BasespaceDl < Formula
     desc = 'Multi-account basespace file downloader'
 
     if OS.mac?
-        url "https://github.com/dweb0/basespace-dl/releases/download/#{version}/basespace-dl-${version}-x86_64-apple-darwin.zip"
+        url "https://github.com/dweb0/basespace-dl/releases/download/#{version}/basespace-dl-#{version}-x86_64-apple-darwin.zip"
         sha256 "6ac17907f83ec9243c48533c1d547ae993a64aca625ed106d45fc8d615e55370"
     elsif OS.linux?
-        url "https://github.com/dweb0/basespace-dl/releases/download/#{version}/basespace-dl-${version}-x86_64-linux.zip"
+        url "https://github.com/dweb0/basespace-dl/releases/download/#{version}/basespace-dl-#{version}-x86_64-linux.zip"
         sha256 "bc32278133b7dc30192ac4171a369767e49ad50a0987e12cc9c0915fbd5194ec"
     end
 
